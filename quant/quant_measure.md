ade0b41d132358c85a8e929e9ff6fbf5daf476ba
Tommy Kimura <tkimura4@illinois.edu>
2022-10-09 22:52:24 -0500

    Initialized basic risk measurments

---
+++ quant/quant_measure.md
@@ -0,0 +1,17 @@
+# Quant - Risk Measurement
+
+## Various Risk Measurement Functions
+
+### Cumulative Return
+
+$$\text{Cumulative Return} = {\text{Current price of security} - \text{Original price of security} \over \text{Original price of security}}$$
+
+### Sharpe Ratio
+
+$$\text{Sharpe Ratio} = {\text{Return of portfolio} - \text{risk-free rate} \over \text{Standard Deviation of the portfolio's excess return}}$$
+
+
+
+### Maximum Drowndown
+
+$$\text{Maximum Drowndown} = {\text{Trough Value} - \text{Peak value} \over \text{Peak value}}$$
---
+++ quant/risk_measure/risk_measure.py
@@ -0,0 +1,36 @@
+'''
+Various risk measurements on StrategyStudio output files
+'''
+
+def cumulative_return(security):
+    '''
+    Measures the Cumulative return of the given security
+    Parameters
+    ----------
+    array : security
+        array representing the security to perform analysis on
+    '''
+    return (security[-1] - security[0]) / security[0]
+
+def sharpe_ratio(security, risk=0.1):
+    '''
+    Measures the Sharpe ratio of the given profit and loss
+    ----------
+    array : security
+        array representing the security to perform analysis on
+    float : risk
+        risk rate for the given security
+    '''
+    return (cumulative_return(security) - risk) / security.std()
+
+def maximum_drowndown(security):
+    '''
+    Measures the Maximum drowndown of the given security
+    ----------
+    array : security
+        array representing the security to perform analysis on
+    '''
+    return (security.min() - security.max()) / security.max()
+
+if __name__ == "__main__":
+    print("Hello World")
