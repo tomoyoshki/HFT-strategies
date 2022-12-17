11dcae093a8ce95cbd14016b6d1d8e1a1c066370
Tommy Kimura <tkimura4@illinois.edu>
2022-12-17 14:13:27 -0600

    Restructured Torch strategy

---
+++ get_torch.sh
@@ -0,0 +1,4 @@
+apt-get install zip
+cd /home/vagrant/ss/sdk/RCM/StrategyStudio/includes/
+wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.13.0%2Bcpu.zip
+unzip libtorch-cxx11-abi-shared-with-deps-1.13.0+cpu.zip
